2b9b33acf86e3d8c17a96768c5b43ef222a0b404
Denis Lukyanov <ohmycto@gmail.com>
2021-01-22 09:32:41 +0300

    Remove error html pages in development.

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :geo_tasks, GeoTasks.Repo,
 # with webpack to recompile .js and .css sources.
 config :geo_tasks, GeoTasksWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
